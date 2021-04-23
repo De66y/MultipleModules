cec22b790ae6c7510991529428a587d4d3cc2f18
De Smits <debbbske@hotmail.com>
2021-04-23 12:42:12 +0200

    wachtwoordgenerator

---
+++ CaseOne/src/main/java/nl/marktplaats/service/Wachtwoordgenerator.java
@@ -0,0 +1,18 @@
+package nl.marktplaats.service;
+
+import java.util.Random;
+
+public class Wachtwoordgenerator {
+
+    public String maakEenRandomWachtwoord() {
+        int vanafA = 97;
+        int totEnMetZ = 122;
+        int wachtwoordLengte = 8;
+        Random random = new Random();
+
+        return random.ints(vanafA, totEnMetZ + 1)
+                .limit(wachtwoordLengte)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+}
