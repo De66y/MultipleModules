5fb49dc9482ffad5fc0b30263f4ac74ce318ee6e
De Smits <debbbske@hotmail.com>
2021-04-23 11:55:20 +0200

    Bezorgwijze toegevoegd

---
+++ CaseOne/src/main/java/nl/marktplaats/service/Bezorgwijze.java
@@ -0,0 +1,24 @@
+package nl.marktplaats.service;
+
+public enum Bezorgwijze {
+    AFHALEN_MAGAZIJN(1, "Afhalen magazijn"),
+    THUIS_AFHALEN_BIJ_VERKOPER(2, "Thuis afhalen bij verkoper"),
+    VERSTUREN(3, "Versturen"),
+    VERSTUREN_ONDER_REMBOURS(4, "Versturen onder rembours");
+
+    private int id;
+    private String omschrijving;
+
+    Bezorgwijze(int id, String omschrijving) {
+        this.id = id;
+        this.omschrijving = omschrijving;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getOmschrijving() {
+        return omschrijving;
+    }
+}
