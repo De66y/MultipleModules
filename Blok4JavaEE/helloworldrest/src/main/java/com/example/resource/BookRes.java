669ca88c77bfdc69ef536d04fe69dcffcbcce931
De Smits <debbbske@hotmail.com>
2021-06-02 23:12:11 +0200

    subresources uitbreiding 6

--- Blok4JavaEE/helloworldrest/src/main/java/com/example/resource/BookRes.java
+++ Blok4JavaEE/helloworldrest/src/main/java/com/example/resource/BookRes.java
@@ -1,12 +1,16 @@
 package com.example.resource;
 
+import com.example.data.BookDao;
 import com.example.model.Book;
 import com.example.util.JsonRes;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.PathParam;
 
 public class BookRes implements JsonRes {
+    @Inject
+    private BookDao bookDao;
 
     @GET
     public Book getBookById(@PathParam("id") int id) {
